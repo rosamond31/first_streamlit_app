e9b7441fa2a34945e1336d1739347e4d47529de5
rosamond31 <144858316+rosamond31@users.noreply.github.com>
2023-09-13 09:55:51 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas
+
 
 streamlit.title('Hello cutiepie')
 streamlit.header('Breakfast Favorites')
@@ -10,4 +10,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
