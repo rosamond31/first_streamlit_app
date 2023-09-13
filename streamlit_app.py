3eff987bc9010d19e05fb3df156121a8c5fd39ec
rosamond31 <144858316+rosamond31@users.noreply.github.com>
2023-09-13 17:23:07 +1000

    Update streamlit_app.py
    insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST
    values ('test')

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,3 +51,5 @@ streamlit.dataframe(my_data_row)
 fruit_choice = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('The user entered ', fruit_choice)
 
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST
+values ('from streamlit')")
